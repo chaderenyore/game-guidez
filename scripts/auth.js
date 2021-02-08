13757d03747f9342c7df9fc3ccb0788cd739e174
Chadere Oghenenyoreme <chaderenyore480@gmail.com>
2021-02-08 22:10:09 +0100

    CCode to Signup New Users

---
+++ scripts/auth.js
@@ -0,0 +1,16 @@
+// signup 
+ const signupForm = document.querySelector('#signup-form') 
+ signupForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const email = signupForm['signup-email'].value;
+      const password = signupForm['signup-password'].value;
+      
+     // signup user
+     auth.createUserWithEmailAndPassword(email, password).then(cred => {
+       console.log(cred.user)
+
+       const modal = document.querySelector('#modal-signup');
+       M.Modal.getInstance(modal).close();
+       signupForm.reset()
+     })
+ })
