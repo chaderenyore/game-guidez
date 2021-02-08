363a75d9a552240d6e22c420508f43bf2057420c
Chadere Oghenenyoreme <chaderenyore480@gmail.com>
2021-02-08 23:09:27 +0100

    Logout of current user Functionality

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,6 +1,6 @@
 // signup 
- const signupForm = document.querySelector('#signup-form') 
- signupForm.addEventListener('submit', (e) => {
+const signupForm = document.querySelector('#signup-form') 
+signupForm.addEventListener('submit', (e) => {
       e.preventDefault();
       const email = signupForm['signup-email'].value;
       const password = signupForm['signup-password'].value;
@@ -13,4 +13,13 @@
        M.Modal.getInstance(modal).close();
        signupForm.reset()
      })
- })
+})
+
+// logout
+const logout = document.querySelector('#logout')
+logout.addEventListener('click', (event) => {
+      event.preventDefault();
+      auth.signOut().then(() => {
+            console.log('User signed out');
+      });
+});
