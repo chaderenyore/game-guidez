4c161211cfaa0f39908d74662ce8f095bf3095d4
Chadere Oghenenyoreme <chaderenyore480@gmail.com>
2021-02-26 23:18:23 +0100

    Integrated Signin Functionality In Authjs

--- scripts/auth.js
+++ scripts/auth.js
@@ -23,3 +23,23 @@ logout.addEventListener('click', (event) => {
             console.log('User signed out');
       });
 });
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      // get user info
+      const email = loginForm['login-email'].value;
+      const password = loginForm['login-password'].value;
+
+      // sign the user in now
+      auth.signInWithEmailAndPassword(email, password).then(cred => {
+            console.log(cred.user);
+      })
+
+      // close signin modal and reset the form
+      const modal = document.querySelector('#modal-login');
+       M.Modal.getInstance(modal).close();
+       loginForm.reset()
+})
